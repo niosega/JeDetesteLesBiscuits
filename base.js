08937bd330cf43b800a51544c014ca657826685c
Bonfante Nicolas <nicolas.bonfante@movesol.com>
2021-05-20 10:19:50 +0200

    Remove useless sleep function.

--- base.js
+++ base.js
@@ -1,14 +1,5 @@
 const PREFIX = "JDLB:";
 
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-        if ((new Date().getTime() - start) > milliseconds){
-            break;
-        }
-    }
-}
-
 function elementReady(selector) {
     return new Promise((resolve, reject) => {
       let el = document.querySelector(selector);
