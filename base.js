e8161b208f5d090fa25f7ab37aa1aadfd65b7a3c
Bonfante Nicolas <nicolas.bonfante@movesol.com>
2021-05-20 19:24:20 +0200

    Change how we get element to remove.

--- base.js
+++ base.js
@@ -1,43 +1,52 @@
-const PREFIX = "JDLB:";
+let classes = [
+  "avia-cookie-consent-wrap"
+];
 
-function elementReady(selector) {
-    return new Promise((resolve, reject) => {
-      let el = document.querySelector(selector);
-      if (el) {
-        resolve(el);
-      }
-      new MutationObserver((mutationRecords, observer) => {
-        Array.from(document.querySelectorAll(selector)).forEach((element) => {
-          resolve(element);
-          observer.disconnect();
-        });
-      })
-        .observe(document.documentElement, {
-          childList: true,
-          subtree: true
-        });
-    });
-}
+let ids = [
+  "onetrust-consent-sdk",
+  "didomi-host",
+  "axeptio_overlay",
+  "notification"
+];
 
-function waitAndRemoveById(id) {
-    elementReady(id).then(elem => {
-      console.log(elem);
-      elem.remove();
-    });
-}
+// In case the element is loaded after the page.
+let observer = new MutationObserver(mutations => {
+  for (let mutation of mutations) {
+    if (mutation.target) {
+      classes.forEach(claz => {
+        if (mutation.target.className == claz) {
+          mutation.target.remove();
+        }
+      });
 
-function process() {
-    const hostname = window.location.hostname;
-    
-    if (hostname.includes("veepee.fr")) {
-      waitAndRemoveById("#onetrust-consent-sdk");
-    } else if (hostname.includes("marmiton.org")) {
-      waitAndRemoveById("#didomi-host");
-    } else if (hostname.includes("quarkslab.com")) {
-      waitAndRemoveById("#axeptio_overlay");
-    } else if (hostname.includes("altaprofits.com")) {
-      waitAndRemoveById("#notification");
+      ids.forEach(id => {
+        if (mutation.target.id == id) {
+          mutation.target.remove();
+        }
+      });
     }
-}
+  }
+});
 
-process();
+let options = {
+  childList: true,
+  attributes: true,
+  subtree: true
+};
+observer.observe(document.body, options);
+
+
+// In case the element in loaded when the page is loaded.
+ids.forEach(id => {
+  var elem = document.getElementById(id);
+  if (elem) {
+    elem.remove();
+  }
+});
+
+classes.forEach(claz => {
+  var elem = document.getElementsByClassName(claz);
+  if (elem) {
+    elem.remove();
+  }
+});
